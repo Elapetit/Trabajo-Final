05ef6c4254f2c7f80ead35073b465cf26c9120a2
Elapetit <gabrielacucucorrea@gmail.com>
2022-05-29 19:38:51 -0500

    marzo

--- Scr/scr_marzo.R
+++ Scr/scr_marzo.R
@@ -3,15 +3,32 @@
 library(tidyverse)
 library(haven)
 #cabecera
+bd_deso_c_mar=read_dta('Input/Marzo.dta/Cabecera - Desocupados.dta')
+bd_carac_c_mar=read_dta('Input/Marzo.dta/Cabecera - Características generales (Personas).dta')
+bd_vivi_c_mar=read_dta('Input/Marzo.dta/Cabecera - Vivienda y Hogares.dta')
+bd_ing_c_mar=read_dta('Input/Marzo.dta/Cabecera - Otros ingresos.dta')
 
+#resto
+bd_deso_r_mar=read_dta('Input/Marzo.dta/Resto - Desocupados.dta')
+bd_carac_r_mar=read_dta('Input/Marzo.dta/Resto - Caracteristicas generales (Personas).dta')
+bd_vivi_r_mar=read_dta('Input/Marzo.dta/Resto - Vivienda y Hogares.dta')
+bd_ing_r_mar=read_dta('Input/Marzo.dta/Resto - Otros ingresos.dta')
 
+#unir bases de datos
+bd1=full_join(x=bd_deso_c_mar, y=bd_deso_r_mar, by=c('DIRECTORIO','SECUENCIA_P','ORDEN'))
+bd2=full_join(x=bd_carac_c_mar, y=bd_carac_r_mar, by=c('DIRECTORIO','SECUENCIA_P','ORDEN'))
+bd3=full_join(x=bd_ing_c_mar, y=bd_ing_r_mar, by=c('DIRECTORIO','SECUENCIA_P','ORDEN'))
+bd4=full_join(x=bd_vivi_c_mar, y=bd_vivi_r_mar, by=c('DIRECTORIO','SECUENCIA_P'))
 
+#left_join
+bd5=left_join(x=bd1, y=bd2, by=c('DIRECTORIO','SECUENCIA_P','ORDEN'))
+bd6=left_join(x=bd5, y=bd3, by=c('DIRECTORIO','SECUENCIA_P','ORDEN'))
+bd_marzo=left_join(x=bd6, y=bd4, by=c('DIRECTORIO','SECUENCIA_P'))
 
-
-
-
-
-
+#exportar
+install.packages('rio')
+library(rio)
+export(bd_marzo, 'base de datos marzo.xlsx')
 
 
 
